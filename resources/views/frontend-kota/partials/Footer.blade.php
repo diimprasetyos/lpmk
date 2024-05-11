be4060cfda39e0f6bef6970ef3fda8faa8f2d74f
diimprasetyos <dimasid290503@gmail.com>
2024-05-11 13:08:02 +0700

    refactor: add landing page LPMK Kota

---
+++ resources/views/frontend-kota/partials/Footer.blade.php
@@ -0,0 +1,13 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+    <div class="footer-legal text-center position-relative">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>UpConstruction</span></strong>. All Rights Reserved
+            </div>
+        </div>
+    </div>
+
+</footer>
+<!-- End Footer -->
---
+++ resources/views/frontend-kota/partials/Header.blade.php
@@ -0,0 +1,103 @@
+<header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+        <a href="index.html" class="logo d-flex align-items-center">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1>LPMK Kota Malang<span>.</span></h1>
+        </a>
+
+        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a href="/">Beranda</a></li>
+                <li class="dropdown"><a href="#"><span>LPMK Kecamatan</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                    <ul>
+                        <li class="dropdown"><a href="#"><span>Kec. Blimbing</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                            <ul>
+                                <li><a href="/not-found">Arjosari</a></li>
+                                <li><a href="/not-found">Balearjosari</a></li>
+                                <li><a href="/not-found">Blimbing</a></li>
+                                <li><a href="/not-found">Bunulrejo</a></li>
+                                <li><a href="/not-found">Jodipan</a></li>
+                                <li><a href="/not-found">Kesatrian</a></li>
+                                <li><a href="/not-found">Pandanwangi</a></li>
+                                <li><a href="/not-found">Polehan</a></li>
+                                <li><a href="/not-found">Polowijen</a></li>
+                                <li><a href="/not-found">Purwantor</a></li>
+                                <li><a href="/not-found">Purwodadi</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#"><span>Kec. Kedungkandang</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                            <ul>
+                                <li><a href="/not-found">Arjowinangun</a></li>
+                                <li><a href="/not-found">Bumiayu</a></li>
+                                <li><a href="/not-found">Buring</a></li>
+                                <li><a href="/not-found">Cemorokandang</a></li>
+                                <li><a href="/not-found">Kedungkandang</a></li>
+                                <li><a href="/not-found">Kotalama</a></li>
+                                <li><a href="/not-found">Lesanpuro</a></li>
+                                <li><a href="/not-found">Madyopuro</a></li>
+                                <li><a href="/not-found">Mergosono</a></li>
+                                <li><a href="/not-found">Sawojajar</a></li>
+                                <li><a href="/not-found">Tlogowaru</a></li>
+                                <li><a href="/not-found">Wonokoyo</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#"><span>Kec. Klojen</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                            <ul>
+                                <li><a href="/not-found">Bareng</a></li>
+                                <li><a href="/not-found">Gadingasri</a></li>
+                                <li><a href="/not-found">Kasin</a></li>
+                                <li><a href="/not-found">Kauman</a></li>
+                                <li><a href="/not-found">Kiduldalem</a></li>
+                                <li><a href="/not-found">Klojen</a></li>
+                                <li><a href="/not-found">Oro-Oro Dowo</a></li>
+                                <li><a href="/not-found">Penanggungan</a></li>
+                                <li><a href="/not-found">Rampal</a></li>
+                                <li><a href="/not-found">Celaket</a></li>
+                                <li><a href="/not-found">Samaan</a></li>
+                                <li><a href="/not-found">Sukoharjo</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#"><span>Kec. Lowokwaru</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                            <ul>
+                                <li><a href="/not-found">Arjosari</a></li>
+                                <li><a href="/not-found">Dinoyo</a></li>
+                                <li><a href="/not-found">Jatimulyo</a></li>
+                                <li><a href="/not-found">Ketawanggede</a></li>
+                                <li><a href="/not-found">Lowokwaru</a></li>
+                                <li><a href="/not-found">Merjosari</a></li>
+                                <li><a href="/not-found">Mojolangu</a></li>
+                                <li><a href="/not-found">Sumbersari</a></li>
+                                <li><a href="/not-found">Tasikmadu</a></li>
+                                <li><a href="/not-found">Tlogomas</a></li>
+                                <li><a href="/not-found">Tulusrejo</a></li>
+                                <li><a href="/not-found">Tunggulwulung</a></li>
+                                <li><a href="/not-found">Tunjungsekar</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#"><span>Kec. Sukun</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                            <ul>
+                                <li><a href="/not-found">Bakalankrajan</a></li>
+                                <li><a href="/not-found">Bandulan</a></li>
+                                <li><a href="/not-found">Bandungrejosari</a></li>
+                                <li><a href="/not-found">Ciptomulyo</a></li>
+                                <li><a href="/not-found">Gadang</a></li>
+                                <li><a href="/not-found">Karangbesuki</a></li>
+                                <li><a href="/not-found">Kebonsari</a></li>
+                                <li><a href="/not-found">Mulyorejo</a></li>
+                                <li><a href="/not-found">Pisangcandi</a></li>
+                                <li><a href="/not-found">Sukun</a></li>
+                                <li><a href="/kecamatan">Tanjungrejo</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Profil</a></li>
+                <li><a href="#">Program</a></li>
+            </ul>
+        </nav><!-- .navbar -->
+    </div>
+</header>
