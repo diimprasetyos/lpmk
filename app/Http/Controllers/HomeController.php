97ccfdc585278a15cdd1c260f89f55b066d3b4c9
pars1vale <2118014@scholar.itn.ac.id>
2024-05-17 16:40:55 +0700

    [feat] Update HomeController to display counts of users, program aspirasi, and accepted

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\ProgramAspirasi;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +27,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin-dashboard.pages.home');
+        $users = User::count();
+        $programaspirasi = ProgramAspirasi::count();
+        $acceptedCount = DB::table('program_aspirasi')
+            ->where('status', 'accepted')
+            ->count();
+        $rejectedCount = DB::table('program_aspirasi')
+            ->where('status', 'rejected')
+            ->count();
+        return view('admin-dashboard.pages.home', ['acceptedCount' => $acceptedCount], compact('users', 'programaspirasi'));
     }
 }
