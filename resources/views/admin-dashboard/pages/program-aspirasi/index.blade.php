32e9554bb8772b3d10b77b3388b92b5a81a8175d
pars1vale <2118014@scholar.itn.ac.id>
2024-06-17 09:07:18 +0700

    fix: table view Data Program Aspirasi

--- resources/views/admin-dashboard/pages/program-aspirasi/index.blade.php
+++ resources/views/admin-dashboard/pages/program-aspirasi/index.blade.php
@@ -6,7 +6,6 @@
 
 @section('content')
   <div class="content">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -20,10 +19,8 @@
             </ol>
           </div>
         </div>
-      </div><!-- /.container-fluid -->
+      </div>
     </section>
-
-    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
@@ -32,107 +29,54 @@
               <div class="card-header">
                 <h3 class="card-title">Program Aspirasi</h3>
               </div>
-              <!-- /.card-header -->
-
               <div class="card-body">
 
                 @if (session('status'))
                   <div class="alert alert-success">{{ session('status') }}</div>
                 @endif
 
-                                <div class="mb-2 w-50">
-                                    <a href="{{ url('/admin/program-aspirasi/create') }}"
-                                        class="btn btn-primary btn-sm btn-flat">Create</a>
-                                    <a href="{{ url('admin/program-aspirasi/print') }}"
-                                        class="btn btn-default btn-sm btn-flat" target="_blank">Print</a>
-                                </div>
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Judul</th>
-                                            <th>Prioritas</th>
-                                            <th>RT</th>
-                                            <th>RW</th>
-                                            <th>Deskripsi</th>
-                                            <th>Perantara</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($programAspirasi as $item)
-                                            <tr>
-                                                <td>{{ $item->id }}</td>
-                                                <td>{{ $item->detailProgramAspirasi->title }}</td>
-                                                <td>{{ $item->detailProgramAspirasi->prioritas }}</td>
-                                                <td>{{ $item->detailProgramAspirasi->rt }}</td>
-                                                <td>{{ $item->detailProgramAspirasi->rw }}</td>
-                                                <td>{{ $item->detailProgramAspirasi->deskripsi }}</td>
-                                                <td>{{ $item->perantara }}</td>
-                                                <td>
-                                                    <form
-                                                        action="{{ url('/admin/program-aspirasi/' . $item->id . '/update-status') }}"
-                                                        method="POST">
-                                                        @csrf
-                                                        @method('PUT')
-                                                        <select name="status"
-                                                            class="custom-select form-control-border border-width-2"
-                                                            onchange="this.form.submit()">
-                                                            <option value="pending"
-                                                                {{ $item->status == 'pending' ? 'selected' : '' }}>Pending
-                                                            </option>
-                                                            <option value="accepted"
-                                                                {{ $item->status == 'accepted' ? 'selected' : '' }}>
-                                                                Accepted</option>
-                                                            <option value="rejected"
-                                                                {{ $item->status == 'rejected' ? 'selected' : '' }}>
-                                                                Rejected</option>
-                                                        </select>
-                                                    </form>
-                                                </td>
-                                                <td>
-                                                    <a href="{{ url('/admin/program-aspirasi/' . $item->id . '/edit') }}"
-                                                        class="btn btn-warning btn-sm mx-2 my-1">Edit</a>
-                                                    <a href="{{ url('/admin/program-aspirasi/' . $item->id . '/delete') }}"
-                                                        class="btn btn-danger btn-sm mx-2 my-1"
-                                                        onclick="return confirm('Apakah anda yakin?')">Delete</a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.card-body -->
-                        </div>
-                        <!-- /.card -->
-                    </div>
-                    <!-- /.col -->
-                    
+                <div class="mb-2 w-50">
+                  <a href="{{ url('/admin/program-aspirasi/create') }}" class="btn btn-primary btn-sm btn-flat">Create</a>
+                  <a href="{{ url('admin/program-aspirasi/print') }}" class="btn btn-default btn-sm btn-flat" target="_blank">Print</a>
                 </div>
                 <table class="table table-bordered table-striped">
                   <thead>
                     <tr>
-                      <th>No.</th>
+                      <th>ID</th>
                       <th>Judul</th>
                       <th>Prioritas</th>
                       <th>RT</th>
                       <th>RW</th>
                       <th>Deskripsi</th>
                       <th>Perantara</th>
+                      <th>Status</th>
                       <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
                     @foreach ($programAspirasi as $item)
                       <tr>
-                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->id }}</td>
                         <td>{{ $item->detailProgramAspirasi->title }}</td>
                         <td>{{ $item->detailProgramAspirasi->prioritas }}</td>
                         <td>{{ $item->detailProgramAspirasi->rt }}</td>
                         <td>{{ $item->detailProgramAspirasi->rw }}</td>
                         <td>{{ $item->detailProgramAspirasi->deskripsi }}</td>
                         <td>{{ $item->perantara }}</td>
+                        <td>
+                          <form action="{{ url('/admin/program-aspirasi/' . $item->id . '/update-status') }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <select name="status" class="custom-select form-control-border border-width-2" onchange="this.form.submit()">
+                              <option value="pending" {{ $item->status == 'pending' ? 'selected' : '' }}>Pending
+                              </option>
+                              <option value="accepted" {{ $item->status == 'accepted' ? 'selected' : '' }}>
+                                Accepted</option>
+                              <option value="rejected" {{ $item->status == 'rejected' ? 'selected' : '' }}>
+                                Rejected</option>
+                            </select>
+                          </form>
+                        </td>
                         <td>
                           <a href="{{ url('/admin/program-aspirasi/' . $item->id . '/edit') }}" class="btn btn-warning btn-sm mx-2 my-1">Edit</a>
                           <a href="{{ url('/admin/program-aspirasi/' . $item->id . '/delete') }}" class="btn btn-danger btn-sm mx-2 my-1"
@@ -143,16 +87,10 @@
                   </tbody>
                 </table>
               </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
       </div>
-      <!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
   </div>
 @endsection
