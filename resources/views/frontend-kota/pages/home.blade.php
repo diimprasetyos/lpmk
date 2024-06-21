4dcdada816e24dc30bd33a45d0b7205f6b84bb94
diimprasetyos <dimasid290503@gmail.com>
2024-06-21 17:40:03 +0700

    refactor: replacing some assets

--- resources/views/frontend-kota/pages/home.blade.php
+++ resources/views/frontend-kota/pages/home.blade.php
@@ -43,7 +43,7 @@
 
 
 
-                <div class="section-header">
+                <div class=" section-header">
             <h2>Program LPMK</h2>
             <p>Daftar Program LPMK Kota Malang</p>
         </div>
@@ -141,126 +141,111 @@
 
     <!-- ======= Recent Blog Posts Section ======= -->
     <section id="recent-blog-posts" class="recent-blog-posts">
-        <div class="container" data-aos="fade-up"">
+        <div class="container" data-aos="fade-up">
 
 
 
-                <div class=" section-header">
-            <h2>UMKM</h2>
-            <p>Daftar Data UMKM LPMK Kota Malang</p>
-        </div>
-
-        <div class="row gy-5">
-
-            <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                <div class="post-item position-relative h-100">
-
-                    <div class="post-img position-relative overflow-hidden">
-                        <img src="assets/img/lpmk-kota/umkm-1.jpg" class="img-fluid" alt="">
-                        <span class="post-date">December 12</span>
-                    </div>
-
-                    <div class="post-content d-flex flex-column">
-
-                        <h3 class="post-title">Keripik Patin Crispy</h3>
-
-                        <div class="meta d-flex align-items-center">
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-person"></i> <span class="ps-2">Julia Parker</span>
-                            </div>
-                            <span class="px-3 text-black-50">/</span>
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-folder2"></i> <span class="ps-2">Politics</span>
-                            </div>
-                        </div>
-
-                        <hr>
-
-                        <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
-
-                    </div>
-
-                </div>
-            </div><!-- End post item -->
-
-            <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
-                <div class="post-item position-relative h-100">
-
-                    <div class="post-img position-relative overflow-hidden">
-                        <img src="assets/img//lpmk-kota/umkm-2.jpg" class="img-fluid" alt="">
-                        <span class="post-date">July 17</span>
-                    </div>
-
-                    <div class="post-content d-flex flex-column">
-
-                        <h3 class="post-title">Kacang Pedas</h3>
-
-                        <div class="meta d-flex align-items-center">
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-person"></i> <span class="ps-2">Mario Douglas</span>
-                            </div>
-                            <span class="px-3 text-black-50">/</span>
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-folder2"></i> <span class="ps-2">Sports</span>
-                            </div>
-                        </div>
-
-                        <hr>
-
-                        <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
-
-                    </div>
-
-                </div>
-            </div><!-- End post item -->
-
-            <div class="col-xl-4 col-md-6">
-                <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="300">
-
-                    <div class="post-img position-relative overflow-hidden">
-                        <img src="assets/img//lpmk-kota/umkm-3.jpeg" class="img-fluid" alt="">
-                        <span class="post-date">September 05</span>
-                    </div>
-
-                    <div class="post-content d-flex flex-column">
-
-                        <h3 class="post-title">Batik Tanjungrejo</h3>
-
-                        <div class="meta d-flex align-items-center">
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-person"></i> <span class="ps-2">Lisa Hunter</span>
-                            </div>
-                            <span class="px-3 text-black-50">/</span>
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-folder2"></i> <span class="ps-2">Economics</span>
-                            </div>
-                        </div>
-
-                        <hr>
-
-                        <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
-
-                    </div>
-
-                </div>
-            </div><!-- End post item -->
-            <div class="container mt-5 text-center justify-content-center items-center">
-                <button type="button" class="btn btn-primary">Lihat Lainnya</button>
+            <div class=" section-header">
+                <h2>UMKM</h2>
+                <p>Daftar Data UMKM LPMK Kota Malang</p>
             </div>
-        </div>
+
+            <div class="row gy-5">
+
+                <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                    <div class="post-item position-relative h-100">
+
+                        <div class="post-img position-relative overflow-hidden">
+                            <img src="assets/img/lpmk-kota/umkm-1.jpg" class="img-fluid" alt="">
+                            <span class="post-date">December 12</span>
+                        </div>
+
+                        <div class="post-content d-flex flex-column">
+
+                            <h3 class="post-title">Keripik Patin Crispy</h3>
+
+                            <div class="meta d-flex align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-clock"></i> <span class="ps-2">2023-5-15</span>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+
+                        </div>
+
+                    </div>
+                </div><!-- End post item -->
+
+                <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                    <div class="post-item position-relative h-100">
+
+                        <div class="post-img position-relative overflow-hidden">
+                            <img src="assets/img//lpmk-kota/umkm-2.jpg" class="img-fluid" alt="">
+                            <span class="post-date">July 17</span>
+                        </div>
+
+                        <div class="post-content d-flex flex-column">
+
+                            <h3 class="post-title">Kacang Pedas</h3>
+
+                            <div class="meta d-flex align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-clock"></i> <span class="ps-2">2023-5-15</span>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+
+                        </div>
+
+                    </div>
+                </div><!-- End post item -->
+
+                <div class="col-xl-4 col-md-6">
+                    <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="300">
+
+                        <div class="post-img position-relative overflow-hidden">
+                            <img src="assets/img//lpmk-kota/umkm-3.jpeg" class="img-fluid" alt="">
+                            <span class="post-date">September 05</span>
+                        </div>
+
+                        <div class="post-content d-flex flex-column">
+
+                            <h3 class="post-title">Batik Tanjungrejo</h3>
+
+                            <div class="meta d-flex align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-clock"></i> <span class="ps-2">2023-5-15</span>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+
+                        </div>
+
+                    </div>
+                </div><!-- End post item -->
+                <div class="container mt-5 text-center justify-content-center items-center">
+                    <button type="button" class="btn btn-primary">Lihat Lainnya</button>
+                </div>
+            </div>
 
         </div>
     </section>
     <!-- End Recent Blog Posts Section -->
     <!-- ======= Recent Blog Posts Section ======= -->
     <section id="recent-blog-posts" class="recent-blog-posts">
-        <div class="container" data-aos="fade-up"">
+        <div class="container" data-aos="fade-up">
 
 
 
                 <div class=" section-header">
             <h2>Berita Terbaru</h2>
-            <p>In commodi voluptatem excepturi quaerat nihil error autem voluptate ut et officia consequuntu</p>
+            <p>berita lpmk kota malang</p>
         </div>
 
         <div class="row gy-5">
@@ -275,15 +260,12 @@
 
                     <div class="post-content d-flex flex-column">
 
-                        <h3 class="post-title">Eum ad dolor et. Autem aut fugiat debitis</h3>
+                        <h3 class="post-title">Giat Linmas Tanjungrejo : Peningkatan Keamanan Ketentraman dan Ketertiban Lingkungan</h3>
 
                         <div class="meta d-flex align-items-center">
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-person"></i> <span class="ps-2">Julia Parker</span>
-                            </div>
                             <span class="px-3 text-black-50">/</span>
                             <div class="d-flex align-items-center">
-                                <i class="bi bi-folder2"></i> <span class="ps-2">Politics</span>
+                                <i class="bi bi-folder2"></i> <span class="ps-2">Kegiatan</span>
                             </div>
                         </div>
 
@@ -300,21 +282,18 @@
                 <div class="post-item position-relative h-100">
 
                     <div class="post-img position-relative overflow-hidden">
-                        <img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt="">
+                        <img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt="">
                         <span class="post-date">July 17</span>
                     </div>
 
                     <div class="post-content d-flex flex-column">
 
-                        <h3 class="post-title">Et repellendus molestiae qui est sed omnis</h3>
+                        <h3 class="post-title">Pembentukan Posyandu Remaja Kelurahan Tanjungrejo</h3>
 
                         <div class="meta d-flex align-items-center">
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-person"></i> <span class="ps-2">Mario Douglas</span>
-                            </div>
                             <span class="px-3 text-black-50">/</span>
                             <div class="d-flex align-items-center">
-                                <i class="bi bi-folder2"></i> <span class="ps-2">Sports</span>
+                                <i class="bi bi-folder2"></i> <span class="ps-2">Kesehatan</span>
                             </div>
                         </div>
 
@@ -331,21 +310,18 @@
                 <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="300">
 
                     <div class="post-img position-relative overflow-hidden">
-                        <img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt="">
+                        <img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt="">
                         <span class="post-date">September 05</span>
                     </div>
 
                     <div class="post-content d-flex flex-column">
 
-                        <h3 class="post-title">Quia assumenda est et veritati tirana ploder</h3>
+                        <h3 class="post-title">Penyaluran BST (Bantuan Sosial Tunai) Kemensos ke 3-4 Tahun 2021</h3>
 
                         <div class="meta d-flex align-items-center">
-                            <div class="d-flex align-items-center">
-                                <i class="bi bi-person"></i> <span class="ps-2">Lisa Hunter</span>
-                            </div>
                             <span class="px-3 text-black-50">/</span>
                             <div class="d-flex align-items-center">
-                                <i class="bi bi-folder2"></i> <span class="ps-2">Economics</span>
+                                <i class="bi bi-folder2"></i> <span class="ps-2">Sosial</span>
                             </div>
                         </div>
 
